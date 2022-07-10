22161e6a9579dced1cbfde31a49b2e0e19ef5459
Aleksey443443 <aleksey443443@gmail.com>
2022-07-10 18:56:57 +0300

    HW2(1)

---
+++ HW2/hello_world.cpp
@@ -0,0 +1,12 @@
+#include "move_me_out.h"
+#include "example.h"
+#include <iostream>
+void todo_move_me_out();
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+
+	example_function();
+	todo_move_me_out();
+	
+	return 0;
+}
